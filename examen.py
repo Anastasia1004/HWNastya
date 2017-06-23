f8e569304108e9df8c7b1a0e94bb13c15f11b3b4
Anastasia1004 <nastyayarina@gmail.com>
2017-06-23 15:11:45 +0300

    экзамен

--- examen.py
+++ examen.py
@@ -16,4 +16,28 @@ def file_countsent():
         for f in files:
             cw.write(f+'\t'+str(countsent(os.path.join(root, f)))+'\n')
 
+def text_data(txt1):
+    topic = re.search(r'<meta content="(.*)" name="topic">', txt1).group(1)
+    author = re.search(r'<meta content="(.*)" name="author">', txt1).group(1)
+    data = [author, topic]
+    return data
+
+def csv(data, name):
+    with open(name, 'a', encoding='cp1251') as f:
+        f.write(data[2]+'\t'+data[0]+'\t'+data[1]+'\n')
+
+def supertable():
+    data1 = []
+    for root, dirs, files in os.walk('news'):
+        for f in files:
+            with open(os.path.join(root, f), 'r', encoding='cp1251') as m:
+                txt = m.read()
+            data = text_data(txt)
+            data.append(f)
+            data1.append(data)
+    for data in data1:
+        csv(data, 'supertable.csv')
+
 file_countsent()
+       
+supertable()
